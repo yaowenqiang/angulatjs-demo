da408d057f810c37e557add50a7373c60880c02f
yaowenqiang <yaowenqiang111@163.com>
2015-11-02 01:35:51 +0800

    add prev and next navgation

--- js/controllers.js
+++ js/controllers.js
@@ -11,5 +11,16 @@ artListControllers.controller('DetailsController',['$scope','$http','$routeParam
         console.log($routeParams.itemId);
         $scope.artists = data;
         $scope.whichItem = $routeParams.itemId;
+        if($routeParams.itemId > 0){
+            $scope.prevItem =  Number($routeParams.itemId) - 1;
+        } else {
+            $scope.prevItem =  $scope.artists.length - 1;
+        }
+
+        if($routeParams.itemId < $scope.artists.length - 1){
+            $scope.nextItem =  Number($routeParams.itemId) + 1;
+        } else {
+            $scope.nextItem =  0;
+        }
     });
 }]);
