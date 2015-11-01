cc4a0a685225429586f4ef5196c52a5706230a7a
yaowenqiang <yaowenqiang111@163.com>
2015-11-01 19:47:38 +0800

    binding to input fields through filters

--- js/controllers.js
+++ js/controllers.js
@@ -2,6 +2,7 @@ var myNameSpace = angular.module('myApp',[]);
 myNameSpace.controller('MyController',['$scope','$http',function($scope,$http){
     $http.get('js/data.json').success(function(data){
         $scope.artists = data;
+        $scope.artistOrder = 'name';
     });
 }]);
 
