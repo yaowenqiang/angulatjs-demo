4307a732ab275a532f2faf25fc3ad8b2641a7623
yaowenqiang <yaowenqiang111@163.com>
2015-11-01 17:54:59 +0800

    add module file

---
+++ js/controllers.js
@@ -0,0 +1,9 @@
+var myNameSpace = angular.module('myApp',[]);
+myNameSpace.controller('MyController',function($scope){
+$scope.author = {
+        'name': "Ray villalobos",
+        'title': 'staff Author',
+        'company': 'lynda.com'
+    };
+});
+
